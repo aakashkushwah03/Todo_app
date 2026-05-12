dab0950913dfb167c2215384e9199a0713963c0b
Aakash Kushwah <140482401+aakashkushwah03@users.noreply.github.com>
2026-05-12 12:32:28 +0530

    Enable debug mode True for app.run

--- app.py
+++ app.py
@@ -143,4 +143,4 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     # app.run(debug = False)
-      app.run(host = '0.0.0.0', debug = False)
+      app.run(host = '0.0.0.0', debug = True)
