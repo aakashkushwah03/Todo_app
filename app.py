dc7f9cd2c8498fd265d9c527b8f6094df647967b
Aakash Kushwah <140482401+aakashkushwah03@users.noreply.github.com>
2026-05-12 12:14:32 +0530

    Change app.run to enable debug mode and set host

--- app.py
+++ app.py
@@ -142,4 +142,5 @@ def delete(sno):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug = False)
+    # app.run(debug = False)
+      app.run(host = '0.0.0.0', debug = False)
